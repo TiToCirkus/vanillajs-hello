07c243213595d0460d4e1b87749501ab043f7c89
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-07-09 02:58:00 +0000

    second commit

--- src/app.js
+++ src/app.js
@@ -8,9 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = () => {
   //write your code here
   document.querySelector("#excuse").innerHTML = generateExcuse();
-  console.log("Hello Rigo from the console!");
 };
 
+function RamdomIndex(array) {
+  let ArrayIndex = Math.floor(Math.random() * array.length);
+  return array[ArrayIndex] + " ";
+}
 let generateExcuse = () => {
   let who = ["The dog", "My grandma", "The mailman", "My bird"];
   let action = ["ate", "peed", "crushed", "broke"];
@@ -23,18 +26,10 @@ let generateExcuse = () => {
     "while I was praying"
   ];
 
-  let whoIndex = Math.floor(Math.random() * who.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let whatIndex = Math.floor(Math.random() * what.length);
-  let whenIndex = Math.floor(Math.random() * when.length);
-
   return (
-    who[whoIndex] +
-    " " +
-    action[actionIndex] +
-    " " +
-    what[whatIndex] +
-    " " +
-    when[whenIndex]
+    RamdomIndex(who) +
+    RamdomIndex(action) +
+    RamdomIndex(what) +
+    RamdomIndex(when)
   );
 };
