9ab1eb134eb08b66f673a8f12a5dfa973a9c0727
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-07-09 03:04:05 +0000

    tercercommit

--- src/app.js
+++ src/app.js
@@ -8,28 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = () => {
   //write your code here
   document.querySelector("#excuse").innerHTML = generateExcuse();
-};
 
-function RamdomIndex(array) {
-  let ArrayIndex = Math.floor(Math.random() * array.length);
-  return array[ArrayIndex] + " ";
-}
-let generateExcuse = () => {
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
-    "before the class",
-    "when I was sleeping",
-    "while I was exercising",
-    "during my lunch",
-    "while I was praying"
-  ];
+  function RamdomIndex(array) {
+    let ArrayIndex = Math.floor(Math.random() * array.length);
+    return array[ArrayIndex] + " ";
+  }
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-  return (
-    RamdomIndex(who) +
-    RamdomIndex(action) +
-    RamdomIndex(what) +
-    RamdomIndex(when)
-  );
+    return (
+      RamdomIndex(who) +
+      RamdomIndex(action) +
+      RamdomIndex(what) +
+      RamdomIndex(when)
+    );
+  }
 };
