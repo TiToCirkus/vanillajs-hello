98522ac71f11f8ce0a43931589004e8a1fac8b73
TiToCirkus <73978802+TiToCirkus@users.noreply.github.com>
2024-07-11 00:42:02 +0000

    cuartocommit

--- src/app.js
+++ src/app.js
@@ -7,29 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = () => {
   //write your code here
-  document.querySelector("#excuse").innerHTML = generateExcuse();
-
   function RamdomIndex(array) {
     let ArrayIndex = Math.floor(Math.random() * array.length);
     return array[ArrayIndex] + " ";
   }
-  function generateExcuse() {
-    let who = ["The dog", "My grandma", "The mailman", "My bird"];
-    let action = ["ate", "peed", "crushed", "broke"];
-    let what = ["my homework", "my phone", "the car"];
-    let when = [
-      "before the class",
-      "when I was sleeping",
-      "while I was exercising",
-      "during my lunch",
-      "while I was praying"
-    ];
 
-    return (
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document.querySelector("#excuse").innerHTML =
+    RamdomIndex(who) +
+    RamdomIndex(action) +
+    RamdomIndex(what) +
+    RamdomIndex(when);
+
+  let excuseButton = document.querySelector("#excuseGenerator");
+
+  excuseButton.addEventListener("click", () => {
+    let newExcuse =
       RamdomIndex(who) +
       RamdomIndex(action) +
       RamdomIndex(what) +
-      RamdomIndex(when)
-    );
-  }
+      RamdomIndex(when);
+    document.querySelector("#excuse").innerHTML = newExcuse;
+  });
 };
